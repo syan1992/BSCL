5c5d755760fea737bd25bd3c50f230c6e3b705e4
syan1992 <331817159@qq.com>
2022-08-25 10:36:38 -0500

    revise loss files

--- loss/loss_scl_cls.py
+++ loss/loss_scl_cls.py
@@ -3,8 +3,7 @@ import torch.nn as nn
 
 
 class SupConLoss(nn.Module):
-    """Supervised Contrastive Learning: https://arxiv.org/pdf/2004.11362.pdf.
-    It also supports the unsupervised contrastive loss in SimCLR"""
+    # Supervised Contrastive Learning: https://arxiv.org/pdf/2004.11362.pdf.
 
     def __init__(self, temperature=0.07, contrast_mode="one", base_temperature=0.07):
         super(SupConLoss, self).__init__()
@@ -13,10 +12,7 @@ class SupConLoss(nn.Module):
         self.base_temperature = base_temperature
 
     def forward(self, features, labels=None, mask=None):
-        """Compute loss for model. If both `labels` and `mask` are None,
-        it degenerates to SimCLR unsupervised loss:
-        https://arxiv.org/pdf/2002.05709.pdf
-
+        """Compute loss for model.
         Args:
             features: hidden vector of shape [bsz, n_views, ...].
             labels: ground truth of shape [bsz].
@@ -25,47 +21,30 @@ class SupConLoss(nn.Module):
         Returns:
             A loss scalar.
         """
-        device = torch.device("cuda") if features.is_cuda else torch.device("cpu")
-
-        if len(features.shape) < 3:
-            raise ValueError(
-                "`features` needs to be [bsz, n_views, ...]," "at least 3 dimensions are required"
-            )
-        if len(features.shape) > 3:
-            features = features.view(features.shape[0], features.shape[1], -1)
+        device = torch.device(
+            "cuda") if features.is_cuda else torch.device("cpu")
 
         contrast_count = 1
         contrast_feature_smiles = features[:, 1, :]
         contrast_feature_graph = features[:, 0, :]
 
         mask_init = mask
-        #######################################################################
+
         ############################anchor graph###############################
-        if self.contrast_mode == "one":
-            anchor_feature = contrast_feature_graph
-            anchor_count = 1
-        elif self.contrast_mode == "all":
-            anchor_feature = contrast_feature
-            anchor_count = contrast_count
-        else:
-            raise ValueError("Unknown mode: {}".format(self.contrast_mode))
 
-        # anchor graph contrast SMILES-------------------------------------------
+        anchor_feature = contrast_feature_graph
+        anchor_count = 1
+
+        # anchor graph contrast SMILES
         batch_size = features.shape[0]
-        if labels is not None and mask_init is not None:
-            raise ValueError("Cannot define both `labels` and `mask`")
-        elif labels is None and mask_init is None:
-            mask = torch.eye(batch_size, dtype=torch.float32).to(device)
-        elif labels is not None:
-            labels = labels.contiguous().view(-1, 1)
-            if labels.shape[0] != batch_size:
-                raise ValueError("Num of labels does not match num of features")
-            mask = torch.eq(labels, labels.T).float().to(device)
-        else:
-            mask = mask.float().to(device)
 
+        labels = labels.contiguous().view(-1, 1)
+        if labels.shape[0] != batch_size:
+            raise ValueError("Num of labels does not match num of features")
+        mask = torch.eq(labels, labels.T).float().to(device)
         anchor_dot_contrast = torch.div(
-            torch.matmul(anchor_feature, contrast_feature_smiles.T), self.temperature
+            torch.matmul(anchor_feature,
+                         contrast_feature_smiles.T), self.temperature
         )
         # for numerical stability
         logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
@@ -94,34 +73,22 @@ class SupConLoss(nn.Module):
         loss = -(self.temperature / self.base_temperature) * mean_log_prob_pos
         loss_graph_smiles = loss.view(anchor_count, -1).mean()
 
-        #######################################################################
         ############################anchor SMILES###############################
-        if self.contrast_mode == "one":
-            anchor_feature = contrast_feature_smiles
-            anchor_count = 1
-        elif self.contrast_mode == "all":
-            anchor_feature = contrast_feature
-            anchor_count = contrast_count
-        else:
-            raise ValueError("Unknown mode: {}".format(self.contrast_mode))
 
-        # -----------------------------------------------------------------------
-        # anchor SMILES contrast graph------------------------------------------
+        anchor_feature = contrast_feature_smiles
+        anchor_count = 1
+
+        # anchor SMILES contrast graph
         batch_size = features.shape[0]
-        if labels is not None and mask_init is not None:
-            raise ValueError("Cannot define both `labels` and `mask`")
-        elif labels is None and mask_init is None:
-            mask = torch.eye(batch_size, dtype=torch.float32).to(device)
-        elif labels is not None:
-            labels = labels.contiguous().view(-1, 1)
-            if labels.shape[0] != batch_size:
-                raise ValueError("Num of labels does not match num of features")
-            mask = torch.eq(labels, labels.T).float().to(device)
-        else:
-            mask = mask.float().to(device)
+
+        labels = labels.contiguous().view(-1, 1)
+        if labels.shape[0] != batch_size:
+            raise ValueError("Num of labels does not match num of features")
+        mask = torch.eq(labels, labels.T).float().to(device)
 
         anchor_dot_contrast = torch.div(
-            torch.matmul(anchor_feature, contrast_feature_graph.T), self.temperature
+            torch.matmul(anchor_feature,
+                         contrast_feature_graph.T), self.temperature
         )
         # for numerical stability
         logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
