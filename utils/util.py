1a1472b003c747d7cfbb3bd9b20ae07e74cbe7d2
syan1992 <331817159@qq.com>
2022-08-25 15:10:40 -0500

    remove accuracy function(useless)

--- utils/util.py
+++ utils/util.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+
 import torch
 import torch.optim as optim
 
@@ -23,27 +24,11 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
 
-def accuracy(output, target, topk=(1,)):
-    """Computes the accuracy over the k top predictions for the specified values of k"""
-    with torch.no_grad():
-        maxk = max(topk)
-        batch_size = target.size(0)
-
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-        res = []
-        for k in topk:
-            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.mul_(100.0 / batch_size))
-        return res
-
-
 def adjust_learning_rate(args, optimizer, epoch, lr):
     if args.cosine:
         eta_min = lr * (args.lr_decay_rate**3)
-        lr = eta_min + (lr - eta_min) * (1 + math.cos(math.pi * epoch / args.epochs)) / 2
+        lr = eta_min + (lr - eta_min) * \
+            (1 + math.cos(math.pi * epoch / args.epochs)) / 2
     else:
         steps = np.sum(epoch > np.asarray(args.lr_decay_epochs))
         if steps > 0:
@@ -55,7 +40,8 @@ def adjust_learning_rate(args, optimizer, epoch, lr):
 
 def warmup_learning_rate(args, epoch, batch_id, total_batches, optimizer):
     if args.warm and epoch <= args.warm_epochs:
-        p = (batch_id + (epoch - 1) * total_batches) / (args.warm_epochs * total_batches)
+        p = (batch_id + (epoch - 1) * total_batches) / \
+            (args.warm_epochs * total_batches)
         lr = args.warmup_from + p * (args.warmup_to - args.warmup_from)
 
         for param_group in optimizer.param_groups:
@@ -64,7 +50,8 @@ def warmup_learning_rate(args, epoch, batch_id, total_batches, optimizer):
 
 def set_optimizer(opt, model):
 
-    optimizer = optim.Adam(model.parameters(), lr=opt.learning_rate, weight_decay=opt.weight_decay)
+    optimizer = optim.Adam(
+        model.parameters(), lr=opt.learning_rate, weight_decay=opt.weight_decay)
 
     return optimizer
 
