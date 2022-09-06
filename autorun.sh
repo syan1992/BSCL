4070b7901281fd180769cbfa86af8c05eebb62a6
syan1992 <331817159@qq.com>
2022-09-06 14:27:58 -0500

    modify the autorun file

--- autorun.sh
+++ autorun.sh
@@ -18,22 +18,22 @@ elif [ $1 == 'bace' ]
 then
 	python main.py --lr_decay_epochs 300 --lr_decay_rate 0.5 --learning_rate 0.0001 --batch_size 128 --epochs 300 \
 		            --trial 1 --dataset bace --num_tasks 1 --temp 0.07 --mlp_layers 1 --classification\
-			        --wscl 1 --wrecon 0.1 --data_dir "datasets" --num_gc_layers 7
+			        --wscl 1 --wrecon 0.1 --data_dir "datasets" --num_gc_layers 7 --global_feature
 elif [ $1 == 'tox21' ]
 then
 	python main.py --lr_decay_epochs 300 --lr_decay_rate 0.5 --learning_rate 0.0001 --batch_size 128 --epochs 300 \
 		            --trial 1 --dataset tox21 --num_tasks 12 --temp 0.07 --mlp_layers 1 --classification\
-                    --wscl 1 --wrecon 0.1 --data_dir "datasets" --num_gc_layers 7
+                    --wscl 1 --wrecon 0.1 --data_dir "datasets" --num_gc_layers 7 --global_feature
 elif [ $1 == 'sider' ]
 then
 	python main.py --lr_decay_epochs 300 --lr_decay_rate 0.5 --learning_rate 0.0001 --batch_size 128 --epochs 300 \
 		            --trial 1 --dataset sider --num_tasks 27 --temp 0.07 --mlp_layers 1 --classification\
-                    --wscl 1 --wrecon 0.1 --data_dir "datasets" --num_gc_layers 7
+                    --wscl 1 --wrecon 0.1 --data_dir "datasets" --num_gc_layers 7 --global_feature
 elif [ $1 == 'clintox' ]
 then	
 	python main.py --lr_decay_epochs 300 --lr_decay_rate 0.5 --learning_rate 0.0001 --batch_size 128 --epochs 100 \
 		            --trial 1 --dataset clintox --num_tasks 2 --temp 0.07 --mlp_layers 2 --classification\
-                    --wscl 1 --wrecon 0.1 --data_dir "datasets" --num_gc_layers 7
+                    --wscl 1 --wrecon 0.1 --data_dir "datasets" --num_gc_layers 7 --global_feature
 else
 	echo "Input a dataset"
 fi
